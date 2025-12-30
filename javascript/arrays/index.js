c44b47de2fb2d72961f7fdef12b9c5c5f3245725
IT-WIBRC <wibrc.se.jc@gmail.com>
2025-12-30 15:04:09 +0100

    exercise: add enhancement exercise of the student list

--- javascript/arrays/index.js
+++ javascript/arrays/index.js
@@ -1,63 +1,104 @@
-const DB_NAME = "devia";
-const DB_VERSION = 1;
-
-const request = await indexedDB.open(DB_NAME, DB_VERSION);
-request.onerror = function () {
-  console.log("error :" + request.error);
-};
-
-request.onsuccess = function () {
-  let db = openRequest.result;
-  db.onversionchange = function () {
-    db.close();
-    alert("Database is outdated, please reload the page.");
-  };
-};
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
 
 const studentList = [];
-const list = document.querySelector(".list");
 const studentName = document.querySelector("input");
-const ajouterBtn = document.querySelector("button");
-
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+const addBtn = document.querySelector("button");
 
 const generateUID = () => {
   return crypto.randomUUID();
 };
 
-function addToDOM(value) {
-  const li = document.createElement("li");
-  li.textContent = value;
-  list.appendChild(li);
-}
-
-function addToStudentList(nameToAdd) {
-  const hasThisStudentAlready = studentList.find(
-    (name) => name.toLowerCase() === nameToAdd.toLowerCase()
+function deleteFromListById(listToRemoveFrom, studentId) {
+  const studentToDeleteIndex = studentList.findIndex(
+    (student) => student.id === studentId
   );
 
-  if (
-    nameToAdd == "" ||
-    nameToAdd === undefined ||
-    hasThisStudentAlready !== undefined
-  ) {
-    studentName.value = "";
-    return;
-  }
-  studentList.push(nameToAdd);
+  if (studentToDeleteIndex === -1) return;
 
-  addToDOM(nameToAdd);
+  studentList.splice(studentToDeleteIndex, 1);
+  listToRemoveFrom.querySelector(`[data-id="${studentId}"]`).remove();
+}
+
+function addDeleteButtonTo(element, label) {
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "delete-btn";
+  deleteButton.textContent = "❌";
+  deleteButton.ariaLabel = label;
+  deleteButton.title = label;
+  element.appendChild(deleteButton);
+}
+
+function insertStudentTo(student, target) {
+  const li = document.createElement("li");
+  li.className = "student";
+  li.textContent = student.name;
+  li.dataset.id = student.id;
+
+  addDeleteButtonTo(li, `Enlever l'etudiant ${student.name} de la liste`);
+
+  li.querySelector("button").addEventListener("click", () => {
+    deleteFromListById(target, student.id);
+  });
+
+  target.appendChild(li);
+}
+
+function refreshList() {
+  const list = document.querySelector(".list");
+  list.replaceChildren();
+  studentList.forEach((student) => insertStudentTo(student, list));
+}
+
+function sortStudentList() {
+  studentList.sort((studentA, studentB) =>
+    studentA.name.toLowerCase().localeCompare(studentB.name.toLowerCase())
+  );
+}
+
+function capitalize(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+}
+
+function clearField() {
   studentName.value = "";
 }
 
-ajouterBtn.addEventListener("click", function () {
-  addToStudentList(studentName.value);
+function addToStudentList(nameToAdd) {
+  const newStudent = {
+    id: generateUID(),
+    name: capitalize(nameToAdd),
+    createdAt: new Date().toString(),
+  };
+  studentList.push(newStudent);
+}
+
+function processAddition(studentName) {
+  const hasThisStudentAlready = studentList.find(
+    (student) =>
+      student.name.toLocaleLowerCase() === studentName.toLocaleLowerCase()
+  );
+
+  const canAddToList = studentName && hasThisStudentAlready === undefined;
+
+  if (!canAddToList) {
+    clearField();
+    return;
+  }
+
+  addToStudentList(studentName);
+  sortStudentList();
+  refreshList();
+  clearField();
+}
+
+addBtn.addEventListener("click", function () {
+  processAddition(studentName.value);
 });
 
 studentName.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
-    addToStudentList(studentName.value);
+    processAddition(studentName.value);
   }
 });
